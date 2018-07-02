75dd6ac7f8d8b2005f169f281043c9a59217e09c
ninastein <ninavstein@t-online.de>
2018-07-02 16:00:56 +0200

    Download bearbeitet

--- download.php
+++ download.php
@@ -7,32 +7,34 @@ if(!isset($_SESSION['userid'])) {
 //Abfrage der Nutzer ID vom Login
 $userid = $_SESSION['userid'];
 
-$directory = '/home/jt049/public_html/webprojekt.storeit/uploads/files/';
-$mimetype = array(
-    'png' => array('image/png'),
-    'jpeg' => array('image/jpeg', 'image/pjpeg'),
-    'gif' => array('image/gif'),
-    'pdf' => array('application/pdf'),
-    'keynote' => array('application/x-iwork-keynote-sffkey'),
-    'pages' => array('application/x-iwork-pages-sffpages'),
-    'numbers' => array('application/x-iwork-numbers-sffnumbers'),
-    'application/vnd.ms-excel',
-    'application/msword',
-    'application/mspowerpoint',
-    'application/zip');
+if(isset($_REQUEST["file"])){
+    // Get parameters
+    $file = urldecode($_REQUEST["file"]); // Decode URL-encoded string
+    $filepath = '/home/jt049/public_html/webprojekt.storeit/uploads/files/'.$file;
+    $mimetype = array(
+        'png' => array('image/png'),
+        'jpeg' => array('image/jpeg', 'image/pjpeg'),
+        'gif' => array('image/gif'),
+        'pdf' => array('application/pdf'),
+        'keynote' => array('application/x-iwork-keynote-sffkey'),
+        'pages' => array('application/x-iwork-pages-sffpages'),
+        'numbers' => array('application/x-iwork-numbers-sffnumbers'),
+        'application/vnd.ms-excel',
+        'application/msword',
+        'application/mspowerpoint',
+        'application/zip');
 
-if(empty($_GET["name"]))
-{
-    echo " keine Datei angegeben";
-    die();
+    // Process download
+    if(file_exists($filepath)) {
+        header("Content-Type:".$mimetype);
+        header('Content-Disposition: attachment; filename="'.basename($filepath).'"');
+        header("Content-Transfer-Encoding: binary ");
+        header('Content-Length: ' . filesize($filepath));
+        flush(); // Flush system output buffer
+        readfile($filepath);
+    }
+    else {
+        echo "Fehlgeschlagen";
+    }
 }
-else
-{
-    $filename=$_GET["name"];
-}
-$filepath=$directory.$filename;
-header("Content-Type:".$mimetype);
-header('Content-Disposition: attachment;name="'.$filename.'"');
-header("Content-Transfer-Encoding: binary ");
-header("Content-Length: ".filesize($filepath));
-readfile($filepath);
+?>
