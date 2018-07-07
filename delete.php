7e3d9ad7744d7c3f8c44c291535abf80f7ebc09e
Linus <lb107@hdm-stuttgart.de>
2018-07-07 15:58:33 +0200

    Teilen per E-Mail: PHP, HTML und CSS; noch nicht funktionsfähig

--- delete.php
+++ delete.php
@@ -1,15 +1,35 @@
-<?php
-include ("connection.php");
-$id =$_REQUEST['id'];
-$sql = "DELETE FROM webprojekt_dateien WHERE id='$id'";
-if(mysqli_query($db, $sql)){
-    echo "Die Datei wurde erfolgreich gelöscht";
-} else{
-    echo "Fehler: Die Datei konnte nicht gelöscht werden. $sql" . mysqli_error($link);
-}
-/**
- * Created by PhpStorm.
- * User: Linus
- * Date: 23.06.2018
- * Time: 16:24
- */
+<head>
+    <link href="alertBox.css" rel="stylesheet">
+
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" rel="stylesheet">
+<style>
+    .congrats {
+        padding: 20px;
+        background-color: #007bff;
+        color: white;
+        border-radius: 5px;
+    }
+</style>
+</head>
+ <?php
+    include ("connection.php");
+    $id = $_REQUEST['id'];
+    $sql = "DELETE FROM dateien WHERE id='$id'";
+    if($dsn ($db, $sql)){
+        ?>
+        <body>
+            <div class="congrats">
+                <span class="closebtn"  onclick="this.parentElement.style.display='none';">&times;</span>
+                <strong>Die Datei wurde erfolgreich gelöscht.</strong><br>
+            </div>
+        </body>
+<?php }
+    else{
+        ?>
+        <body>
+            <div class="alert">
+                <span class="closebtn"  onclick="this.parentElement.style.display='none';">&times;</span>
+                <strong>Die Datei konnte leider nicht gelöscht werden!</strong><br>
+            </div>
+        </body>
+<?php } ?>
--- mailshare_button.php
+++ mailshare_button.php
@@ -10,7 +10,6 @@
     <title></title>
 
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" rel="stylesheet">
-    <link href="signin.css" rel="stylesheet">
     <link href="alertBox.css" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
 
@@ -21,19 +20,23 @@
             background-color: #00b6ff;
             color: white;
             border-radius: 5px;
-            width: 35%;
             display: none;
-            padding:10px;
+            padding:0.7em;
+            width: 100%;
+            min-width: 50px;
+            max-width: 490px;
         }
         .btn-md {
-            margin: 5px;
+            margin-left: 5px;
+            display: inline;
         }
         .share-btn {
             display: inline-block;
             color: #ffffff;
             border: none;
             padding: 0.5em;
-            width: 4em;
+            width: 2.5em;
+            height: 2em;
             box-shadow: 0 2px 0 0 rgba(0,0,0,0.2);
             outline: none;
             text-align: center;
@@ -54,31 +57,65 @@
             background: #007bff;
             border-radius: 5px;
         }
+        .form-control {
+            width: 100%;
+            max-width: 200px;
+            min-width: 27px;
+        }
+        .closebtn {
+            color: white;
+            font-weight: bold;
+            float: right;
+            font-size: 22px;
+            line-height: 20px;
+            cursor: pointer;
+            transition: 0.3s;
+            margin: 0;
+        }
     </style>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 </head>
 
 <body>
-<a href="" class="share-btn email" id="email-but">
+<a href="" class="share-btn email">
     <i class="fa fa-envelope" aria-hidden="true"></i>
 </a>
-<div class="alert" id="formular" >
-    <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
+<div class="alert " >
+    <span class="closebtn form-inline" onclick="this.parentElement.style.display='none';">&times;</span>
     <div class="col-md-8">
-            <form action="?mailshare=1" method="post" class="form-inline">
+        <form action="?mailshare=1" method="post" class="form-inline">
                 <div>
                     <p><strong>Mit anderen teilen:</strong></p>
-                    <input type="email" class="form-control" placeholder="E-Mail Addresse" name="email" required autofocus>
+                    <input type="email" name="email" class="form-control" placeholder="E-Mail Addresse"  required autofocus>
                     <button class="btn btn-md btn-primary" type="submit" name="share">Teilen</button>
                 </div>
             </form>
     </div>
 </div>
+
 <script>
-    $("#email-but").click(function(){
-            $("#formular").toggle();
+
+    $(".share-btn").click(function(){
+        $(this).nextUntil('.share-btn').toggle();
         event.preventDefault();
     });
+
+    /*$('.share-btn').first().click(function() {
+       $('.alert').toggle();
+       event.preventDefault();// Do somehting
+   });
+    $(".share-btn").on('click', function(){
+            $(this).find('.alert').toggle();
+        event.preventDefault();
+    });
+   $('.share-btn').click(function(e){
+        $(e.alert).toggle();
+    });
+    $(this).parent('.share-btn').find('.alert').stop().toggle();
+
+    $('.share-btn').on('click', '.alert' , function(){
+        $(this).toggle(); // hides only the element that was clicked with the class .the-class
+    });*/
 </script>
 </body>
 <?php
@@ -87,19 +124,19 @@ if(isset($_GET['mailshare'])) {
     $empfaenger = "{$email}";
 
     /*echo $empfaenger;*/
+    $absender = "SELECT email FROM webprojekt WHERE userid ='".$_SESSION["userid"]."'";
     $betreff = "Filesharing mit store.it";
-    $mailtext = "Linus hat die Datei {$dateiname} mit dir geteilt.";
+    $id = $_REQUEST['id'];
+    $dateiname = "SELECT name FROM dateien WHERE id ='$id'".$_SESSION["userid"]."'"; //SO NUR EVTL RICHTIG: id muss mit dateiname verknüpft sein
+    $mailtext = "{$absender} hat die Datei {$dateiname} mit dir geteilt.";
     $mailtext .= "Klicke auf den Link, um sie herunterzuladen. DIRECT_DOWNLOAD LINK-URL";
-    $absender = "{lb107@hdm-stuttgart.de}";
 
     $headers = array();
     $headers[] = "MIME-Version: 1.0";
     $headers[] = "Content-type: text/plain; charset=utf-8";
-    $headers[] = "From: {lb107@hdm-stuttgart.de}";
-// falls Bcc benötigt wird
-    /*$headers[] = "Bcc: <mitleser@example.com>";*/
-    $headers[] = "Reply-To: {lb107@hdm-stuttgart.de}";
-    $headers[] = "Subject: {$betreff}";
+    $headers[] = "Von: {$absender}";
+    $headers[] = "Antwort an: {$absender}";
+    $headers[] = "Betreff: {$betreff}";
     $headers[] = "X-Mailer: PHP/" . phpversion();
 
     mail($empfaenger, $betreff, $mailtext, implode("\r\n", $headers));
