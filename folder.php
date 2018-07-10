febf3b2c9b68db88594170531c76734fa97a691c
ninastein <ninavstein@t-online.de>
2018-07-10 13:29:24 +0200

    Ordner erstellen

--- folder.php
+++ folder.php
@@ -1,9 +1,26 @@
 <?php
-$directory = '/home/jt049/public_html/webprojekt.storeit/uploads/files/';
+session_start();
+include "connection.php";
+$userid = $_SESSION ['userid'];
+
+$name=$_POST['ordnername'];
+$statement= $db ->prepare("INSERT INTO ordner (name, user_id) VALUES('$name','$userid')");
+$statement ->bindParam(1,$name);
+$statement ->bindParam(2,$userid);
+if (!$statement->execute()){
+    echo "Datenbank-Fehler:";
+    echo $statement->errorInfo()[1];
+    echo $statement->queryString;
+    die();}
+echo 'Datei erfolgreich hochgeladen';
+
+
+/*$directory = '/home/jt049/public_html/webprojekt.storeit/uploads/files/';
 
 $folder=$_POST["ordnername"];
 $folderpath=$directory.$folder;
-if (!file_exists($directory)) {
-    mkdir($directory);
-}
+
+if (!file_exists($folderpath)) {
+    mkdir($folderpath);
+}*/
 ?>
